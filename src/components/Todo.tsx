a30e99658107a6b82c7a6f8894db331a5972c86c
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2021-11-09 15:02:42 +0300

    test1

---
+++ src/components/Todo.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const Todo = ({text, todo, todos, setTodos}) => {
+
+
+    const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+    };
+
+    const completeHandler = () => {
+        setTodos(
+            todos.map((item) => {
+            if (item.id === todo.id) {
+                return { 
+                    ... item,
+                     completed: ! item.completed,
+                };
+            }
+                return item;
+        })
+        );
+    };
+    return(
+        <div className="todo">
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+        <button onClick={completeHandler}className="complete-bth"><i className="fas fa-check"></i></button>
+        <button onClick={deleteHandler} className="trash-bth"><i className="fas fa-trash"></i></button>
+        </div>
+    );
+}
+
+
+export default Todo;
